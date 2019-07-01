02190a143b5d111eef07cb9d1f7032700b360a2a
laijins <710921398@qq.com>
2019-07-01 15:19:31 +0800

    修改controller更新库表

--- clawer-core/src/main/java/com/webmagic/web/StartUpController.java
+++ clawer-core/src/main/java/com/webmagic/web/StartUpController.java
@@ -1,6 +1,7 @@
 package com.webmagic.web;
 
 import com.alibaba.fastjson.JSON;
+import com.webmagic.dao.VcmClawerTaskDao;
 import com.webmagic.job.BaseTask;
 import com.webmagic.job.ScheduleTaskConfig;
 import com.webmagic.job.TaskConfig;
@@ -18,6 +19,7 @@ import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -33,6 +35,8 @@ public class StartUpController {
     CboooVideoMatchProcessor cboooVideoMatchProcessor;
     @Autowired
     UserAgentUtil userAgentUtil;
+    @Autowired
+    VcmClawerTaskDao vcmClawerTaskDao;
 
     @GetMapping("/mediaMatch")
     public String matchVideo(@RequestParam("keyWord") String keyWord) {
@@ -81,36 +85,86 @@ public class StartUpController {
         spider.startRequest(list).thread(1).run();
         return "开始爬虫=" + spider.getPageCount();
     }
+    /**
+     *
+     * @return
+     */
     @GetMapping("/getTaskInfo")
     public String getTaskInfo() {
         log.info("当前爬取任务列表如下：");
         StringBuffer sb = new StringBuffer();
 
-        TaskConfig.getTasks().stream().forEach((BaseTask printBask) -> log.info(printBask.toString(),sb.append(printBask.toString()).append("\n")));
-        return sb.toString().replaceAll("]","]\r\n");
+        TaskConfig.getTasks().stream().forEach((BaseTask printBask) -> log.info(printBask.toString(), sb.append(printBask.toString()).append("\n")));
+        return sb.toString().replaceAll("]", "]\r\n");
 
     }
 
+    /**
+     *
+     * @param taskId
+     * @param frequencyNum
+     * @param reptileDate
+     * @return
+     */
     @GetMapping("/changeTask")
-    public ScheduleTaskConfig.STATUS changeTaskInfo(@RequestParam("taskId") String taskId,@RequestParam("expression") String expression) {
-        log.info("当前爬取任务列表如下：");
-        StringBuffer sb = new StringBuffer();
+    public ScheduleTaskConfig.STATUS changeTaskInfo(@RequestParam("taskId") int taskId, @RequestParam("frequencyNum") int frequencyNum, @RequestParam("reptileDate") String reptileDate) {
+        log.info("修改任务执行周期，taskId="+taskId);
+        String expression = null;
+        Map<String,Object> map = new HashMap<String,Object>();
+        String[] dateInfo = reptileDate.split(":");
+        StringBuffer BufferCron = new StringBuffer();
 
-        BaseTask baseTask = TaskConfig.getTask(taskId);
-        if (baseTask == null || expression == null) {
-            return ScheduleTaskConfig.STATUS.TASK_NOT_EXISTS;        }
-        log.info("change trigger expression:(id=" + taskId + ",expression=" + expression + ")");
+        if(frequencyNum <= 0 || reptileDate == null){
+            log.info("frequencyNum <=0 || reptileDate = null");
+            return ScheduleTaskConfig.STATUS.FAILURE;
+        }
+        BaseTask baseTask = TaskConfig.getTask(taskId+"");
+        if (baseTask == null ) {
+            return ScheduleTaskConfig.STATUS.TASK_NOT_EXISTS;
+        }
+        BufferCron.append("0")
+                .append(" ")
+                .append(Integer.parseInt(dateInfo[1]))
+                .append(" ")
+                .append(Integer.parseInt(dateInfo[0]))
+                .append(" ")
+                .append("*/" + frequencyNum)
+                .append(" * ?");
+        expression = BufferCron.toString();
+        log.info("change trigger expression:(taskId=" + taskId + ",expression=" + expression + ")");
         baseTask.setExpression(expression);
+        if(baseTask.getExpression().equals(expression)){
+            map.put("frequencyNum",frequencyNum);
+            map.put("taskId",taskId);
+            map.put("reptileDate",reptileDate);
+            map.put("updateUser","0");
+            try {
+                vcmClawerTaskDao.updateTaskInfo(map);
+            }catch (Exception e){
+                log.error("changeTask update db error.exception=",e);
+            }
+        }
         return ScheduleTaskConfig.STATUS.SUCCESS;
     }
+
+    /**
+     *
+     * @param taskId
+     * @return
+     */
     @GetMapping("/cancelTask")
     public ScheduleTaskConfig.STATUS cancelTask(@RequestParam("taskId") String taskId) {
+        Map<String,Object> map = new HashMap<String,Object>();
+
         if (!TaskConfig.containsTask(taskId)) {
             return ScheduleTaskConfig.STATUS.TASK_NOT_EXISTS;
         }
         try {
             log.info("cancel task:" + taskId);
             TaskConfig.removeTask(taskId).getScheduledTask().cancel();
+            map.put("taskId",taskId);
+            map.put("status",2);
+            vcmClawerTaskDao.updateTaskInfo(map);
         } catch (Exception e) {
             log.error("取消任务失败:" + taskId, e);
         }
